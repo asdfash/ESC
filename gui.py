0d1542aa4f7927e1197c9560e81c7f88954cce38
asdfash <jit.tham@gmail.com>
2022-07-21 18:02:48 +0800

    edited gui.py

--- gui.py
+++ gui.py
@@ -17,56 +17,111 @@ excelpath = ""
 gdbpath = ""
 outputpath = defauloutput
 logpath= defaultlog
+keyword = "STRATA"
+header = "NUM_TYPE"
 
 try:
     f = open("config.txt","r")
-    configtext =f.read().split(" ")
+    ls = f.readlines()
+    excelpath = ls[0].removesuffix("\n")
+    gdbpath= ls[1].removesuffix("\n")
+    outputpath= ls[2].removesuffix("\n")
+    logpath= ls[3].removesuffix("\n")
     f.close()
-    excelpath = configtext[0]
-    gdbpath= configtext[1]
-    outputpath= configtext[2]
-    logpath = configtext[3]
-    
 except:
     print("error, rebuilding config file with defaults")
     f = open("config.txt","w")
-    f.write(excelpath + " " + gdbpath + " " + outputpath + " " + logpath)
-    f.close()
+    try:
+        f.write(excelpath)
+        f.write('\n')
+        f.write(gdbpath)
+        f.write('\n')
+        f.write(outputpath)
+        f.write('\n')
+        f.write(logpath)
+        f.close()
+    except:
+        print("config saving error")
+        f.close()
 
 #helper functions
 def runhelp():
-    functions.main(excelpath,gdbpath,outputpath,logpath)
+    functions.main(excelpath,gdbpath,outputpath,logpath,keywordtext['text'],headertext['text'])
 
 def excelask():
     global excelpath
     excelpath = filedialog.askopenfilename(initialdir=workingdir, filetypes=[("Excel files", "*.xlsx")])
-    f = open("config.txt","w+")
-    f.write(excelpath + " " + gdbpath + " "+ outputpath + " " + logpath)
-    f.close()
+    f = open("config.txt","w")
+    try:
+        f.write(excelpath)
+        f.write('\n')
+        f.write(gdbpath)
+        f.write('\n')
+        f.write(outputpath)
+        f.write('\n')
+        f.write(logpath)
+        f.close()
+    except:
+        print("config saving error")
+        f.close()
+
     excelpathtext['text'] = excelpath
 
 def gdbask():
     global gdbpath
-    gdbpath = filedialog.askdirectory(initialdir=workingdir)
-    f = open("config.txt","w+")
-    f.write(excelpath + " " + gdbpath + " "+ outputpath + " " + logpath)
-    f.close()
+    gdbpath = filedialog.askopenfilename(initialdir=workingdir, filetypes=[("GDB","gdb")]).removesuffix(".gdb")
+    f = open("config.txt","w")
+    try:
+        f.write(excelpath)
+        f.write('\n')
+        f.write(gdbpath)
+        f.write('\n')
+        f.write(outputpath)
+        f.write('\n')
+        f.write(logpath)
+        f.close()
+    except:
+        print("config saving error")
+        f.close()
+
     gdbpathtext['text'] = gdbpath
 
 def outputask():
     global outputpath
     outputpath = filedialog.askdirectory(initialdir=defauloutput) + '/'
-    f = open("config.txt","w+")
-    f.write(excelpath + " " + gdbpath + " "+ outputpath + " " + logpath)
-    f.close()
+    f = open("config.txt","w")
+    try:
+        f.write(excelpath)
+        f.write('\n')
+        f.write(gdbpath)
+        f.write('\n')
+        f.write(outputpath)
+        f.write('\n')
+        f.write(logpath)
+        f.close()
+    except:
+        print("config saving error")
+        f.close()
+
     outputpathtext['text'] = outputpath
 
 def logask():
     global logpath
     logpath = filedialog.askdirectory(initialdir=workingdir) + '/'
-    f = open("config.txt","w+")
-    f.write(excelpath + " " + gdbpath + " "+ outputpath + " " + logpath)
-    f.close()
+    f = open("config.txt","w")
+    try:
+        f.write(excelpath)
+        f.write('\n')
+        f.write(gdbpath)
+        f.write('\n')
+        f.write(outputpath)
+        f.write('\n')
+        f.write(logpath)
+        f.close()
+    except:
+        print("config saving error")
+        f.close()
+
     logpathtext['text'] = logpath
 
 #draw windows
@@ -81,6 +136,7 @@ excelpathtext = ttk.Label(master =excelframelabel, text=excelpath,width=50)
 excelpathtext.pack()
 excelframelabel.pack(side=tk.LEFT)
 excelbutton = ttk.Button( master =excelframebutton, text="select file",width=15,command=excelask)
+
 excelbutton.pack()
 excelframebutton.pack(side=tk.RIGHT)
 excelframemain.pack(fill=tk.BOTH)
@@ -124,8 +180,25 @@ logbutton.pack()
 logframebutton.pack(side=tk.RIGHT)
 logframemain.pack(fill=tk.BOTH)
 
+keywordframemain = ttk.Frame(master=mainframe)
+keywordframelabel= ttk.Frame(master=keywordframemain)
+keywordframebutton = ttk.Frame(master=keywordframemain)
+keywordlabel = ttk.Label(master =keywordframelabel, text="keyword to search (leave blank if none)",width=50,padding=5)
+keywordlabel.pack()
+keywordtext = ttk.Entry(master =keywordframelabel,width=25)
+keywordtext.pack()
+keywordframelabel.pack(side=tk.LEFT)
+headerlabel = ttk.Label(master =keywordframebutton, text="Column name to search (leave blank if none)",width=50,padding=5)
+headerlabel.pack()
+headertext = ttk.Entry(master =keywordframebutton,width=25)
+headertext.pack()
+keywordframebutton.pack(side=tk.RIGHT)
+keywordframemain.pack(fill=tk.BOTH)
+
 run = ttk.Button(text="run",master=mainframe,command=runhelp)
 run.pack(fill= tk.BOTH)
 mainframe.pack(fill=tk.BOTH)
+keywordtext.insert(0,keyword)
+headertext.insert(0,header)
 root.mainloop()
 
