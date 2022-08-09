be475153f4d63c487a71313d19e649ea31f5ace1
Yu Qin <90382148+M-Rues@users.noreply.github.com>
2022-08-10 01:36:53 +0800

    added column and row fuzzing

--- Tests/fuzz.py
+++ Tests/fuzz.py
@@ -39,13 +39,44 @@ def randomFuzzing(exceldata, gdbdata):
         if(dataout2.shape[0] == gdbdata.shape[0]):
             break
 
+def columnFuzzing(exceldata, gdbdata):
+    data = copy.deepcopy(exceldata)
+    num_col = exceldata.shape[1]
+    i = 0
+    while(True):
+        print(data)
+        col_list = list(data)
+        randint1 = random.randint(0, num_col-1)
+        randint2 = random.randint(0, num_col-1)
+        col_list[randint1], col_list[randint2] = col_list[randint2], col_list[randint1]
+        data.columns = col_list
+        dataout1, dataout2 = compare(gdbdata, data)
+        i+=1
+        print(dataout2.shape[0], i)
+
+def rowFuzzing(exceldata, gdbdata):
+    data = copy.deepcopy(exceldata)
+    num_data = exceldata.shape[0]
+    i = 0
+    while(True):
+        print(data)
+        col_list = list(data)
+        randint1 = random.randint(0, num_data-1)
+        randint2 = random.randint(0, num_data-1)
+        row1, row2 = data.iloc[randint1], data.iloc[randint2]
+        data.iloc[0], data.iloc[1] = row2, row1
+        dataout1, dataout2 = compare(gdbdata, data)
+        i+=1
+        print(dataout2.shape[0], i)
+    
 arcpy.env.workspace = gdbpath            
 exceldata = excelfile(excelpath)
 gdb = gdbdata(gdbpath)
 gdb = gdb[gdb["NUM_TYPE"]== "STRATA"]
 # fuzzing(exceldata, gdb)
-randomFuzzing(exceldata, gdb)
-
+# randomFuzzing(exceldata, gdb)
+# columnFuzzing(exceldata, gdb)
+rowFuzzing(exceldata, gdb)
 
 
 
