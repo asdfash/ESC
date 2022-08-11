5b481f94941dad149dcca443e5df7117ccc2db3f
kayiac <const.cjn@gmail.com>
2022-08-11 15:44:20 +0800

    Update test cases

--- test_dataframe_difference.py
+++ test_dataframe_difference.py
@@ -17,8 +17,17 @@ class dataframeDifferenceTestCases(unittest.TestCase):
     def test_different_dataframe_size(self):
         df1 = pd.DataFrame([[1, 2, 3],[4, 5, 6]])
         df2 = pd.DataFrame([[4, 5, 6]])
+        difference = dataframe_difference(df1, df2).drop("_merge", axis=1)
+        self.assertTrue(difference.equals(pd.DataFrame([[1, 2, 3]])))
+
+    def test_different_data(self):
+        df1 = pd.DataFrame([[0, 0, 0], [1, 2, 3],[4, 5, 6]])
+        df2 = pd.DataFrame([[0, 0, 0], [7, 8, 9],[10, 11, 12]])
         difference = dataframe_difference(df1, df2)
-        self.assertTrue(len(difference)==1)
+        right=difference.loc[difference["_merge"]=="right_only", :].drop("_merge", axis=1).reset_index(drop=True)
+        left=difference.loc[difference["_merge"]=="left_only", :].drop("_merge", axis=1).reset_index(drop=True)
+        self.assertTrue(left.equals(pd.DataFrame([[1, 2, 3], [4, 5, 6]])))
+        self.assertTrue(right.equals(pd.DataFrame([[7, 8, 9], [10, 11, 12]])))
 
 
     
